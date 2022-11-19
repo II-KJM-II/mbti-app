e6015b93a7ac9184b334624d28ac42085905aa12
kangaroo19 <86513078+kangaroo19@users.noreply.github.com>
2022-11-19 18:04:02 +0900

    123

---
+++ src/pages/Home.js
@@ -0,0 +1,57 @@
+import React from 'react'
+import styled from 'styled-components'
+import img from '../img/커닝시티.PNG'
+import Button from 'react-bootstrap/Button'
+import {useNavigate} from 'react-router-dom'
+function Home(){
+    const navigate=useNavigate()
+    const handleClickButton=()=>{ //버튼 클릭시 question화면으로 넘어감
+        navigate('/question')
+    }
+    return (
+        <>
+         <Wrapper>
+            <Header>예비집사 판별기</Header>
+            <Contents>
+                <Title>나에게 맞는 주인님은?</Title>
+                <LogoImage>
+                    <img src={img} alt="" width={350} height={350} className='rounded-circle' />
+                </LogoImage>
+                <Desc>MBTI를 기반으로 하는 나와 잘맞는 고양이 찾기</Desc>
+                <Button onClick={handleClickButton}>테스트 시작</Button>
+            </Contents>
+         </Wrapper>
+        </>
+    )
+}
+
+export default Home
+
+const Wrapper=styled.div`
+    height:100vh;
+    width:100%;
+`
+const Header=styled.div`
+    font-size:40pt;
+    display:flex;
+    justify-content:center;
+    align-items:center;
+`
+const Title=styled.div`
+    font-size:30pt;
+    margin-top:40px;
+`
+const LogoImage=styled.div`
+    margin-top:10px;
+`
+
+const Desc=styled.div`
+    font-size:20pt;
+    margin-top:20px;
+`
+const Contents=styled.div`
+    display:flex;
+    justify-content:center;
+    align-items:center;
+    flex-direction:column
+`
---
+++ src/pages/Question.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import styled from 'styled-components'
+import {ProgressBar,Button} from 'react-bootstrap'
+import {QuestionData} from '../data/questiondata'
+function Question(){
+    return (
+        <Wrapper>
+            <ProgressBar variant="info" now={20} style={{marginTop:'20px'}}/>
+            <Title>{QuestionData[0].title}</Title>
+            <ButtonGroup>
+                <Button style={{width:'40%',minHeigth:'200px',fontSize:'15pt'}}>{QuestionData[0].answera}</Button>
+                <Button style={{width:'40%',minHeigth:'200px',fontSize:'15pt',marginLeft:'20px'}}>{QuestionData[0].answerb}</Button>
+            </ButtonGroup>
+        </Wrapper>
+    )
+}
+
+export default Question
+
+const Wrapper=styled.div`
+    height:100vh;
+    width:100%;
+`
+const Title=styled.div`
+    font-size:30pt;
+    text-align:center;
+`
+const ButtonGroup=styled.div`
+    display:flex;
+    flex-direction:row;
+    align-items:center;
+    justify-content:center;
+`
